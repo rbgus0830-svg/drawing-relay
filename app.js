0c6bfbd0777a1bb62e0258d01b576bc7434427d3
kh <rbgus0830@gmail.com>
2026-09-22 00:20:21 +0900

    개발 시작(0922 00:20)

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+import { loginAnonymously, db, ref, set } from "./js/firebase.js";
+
+async function startApp() {
+  try {
+    const user = await loginAnonymously();
+
+    console.log("Firebase 로그인 성공");
+    console.log("사용자 ID:", user.uid);
+
+    await set(ref(db, `test/${user.uid}`), {
+      message: "그림 릴레이 Firebase 연결 테스트",
+      createdAt: Date.now()
+    });
+
+    console.log("Realtime Database 저장 성공");
+  } catch (error) {
+    console.error("Firebase 로그인 또는 DB 저장 실패:", error);
+  }
+}
+
+startApp();
+
 const classGameBtn = document.getElementById("classGameBtn");
 const groupGameBtn = document.getElementById("groupGameBtn");
 
---
+++ js/firebase.js
@@ -0,0 +1,32 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import {
+  getAuth,
+  signInAnonymously
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+import {
+  getDatabase,
+  ref,
+  set
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAobskDC4Aghi9dcSCS5WdZg0zklG_5gKs",
+  authDomain: "drawing-relay-81ab0.firebaseapp.com",
+  databaseURL: "https://drawing-relay-81ab0-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "drawing-relay-81ab0",
+  storageBucket: "drawing-relay-81ab0.firebasestorage.app",
+  messagingSenderId: "882192977442",
+  appId: "1:882192977442:web:4e7b7dbd35d1e772a6847b"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+
+export { ref, set };
+
+export async function loginAnonymously() {
+  const result = await signInAnonymously(auth);
+  return result.user;
+}
