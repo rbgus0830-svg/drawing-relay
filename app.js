cd32793a77ef5430b3f2e3f9c11e7455201d12c3
kh <rbgus0830@gmail.com>
2026-09-21 23:54:06 +0900

    기본 웹앱 구조 생성

---
+++ app.js
@@ -0,0 +1,10 @@
+const classGameBtn = document.getElementById("classGameBtn");
+const groupGameBtn = document.getElementById("groupGameBtn");
+
+classGameBtn.addEventListener("click", () => {
+  alert("학급 게임을 준비합니다.");
+});
+
+groupGameBtn.addEventListener("click", () => {
+  alert("모둠 게임을 준비합니다.");
+});
